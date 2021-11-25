5a6e780a6334c29c13cc23f2cf75759891006a98
Alex Lillo <alex.lillo@healthhero.com>
2021-11-25 14:12:31 +0000

    eslint config

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -3,11 +3,17 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
-    plugins: [
-      '@snowpack/plugin-sass',
-    ],
-    mount: {
-      public: '/',
-      src: '/dist',
-    }
-  };
+  plugins: [
+    '@snowpack/plugin-sass'
+    // '@snowpack/plugin-run-script',
+    // {
+    //   cmd: 'eslint src --ext .js,.jsx,.ts,.tsx',
+    //   // Optional: Use npm package "eslint-watch" to run on every file change
+    //   // watch: 'esw -w --clear src --ext .js,.jsx,.ts,.tsx'
+    // },
+  ],
+  mount: {
+    public: '/',
+    src: '/dist'
+  }
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
 // import data from '../data/planes.json'
 
 // const populateSearchSelect = () => {
-    
-// }
 
+// }
 
 // // Get stuff loaded
 // document.onreadystatechange = function () {
@@ -11,13 +10,11 @@
 //     console.log(data.data);
 //     // check the value - if it's 'interactive' then the DOM has loaded
 //     // if (document.readyState === "interactive") {
-        
-        
+
 //     // }
 // }
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
