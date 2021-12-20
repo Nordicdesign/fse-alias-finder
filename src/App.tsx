3824e2c2b53417cd2833b1107ab1942c05c35226
Alex Lillo <alex.lillo@gmail.com>
2021-12-20 23:45:35 +0000

    fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import './assets/styles/app.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
