43f72973e378e26c0b965357302d323193637958
Alex Lillo <alex.lillo@gmail.com>
2024-04-16 20:36:23 +0100

    make it beta

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <header>
       <h1>
         <Link to="/">
-          <em>FSE</em> Alias Finder <sup>Alpha</sup>
+          <em>FSE</em> Alias Finder <sup>Beta</sup>
         </Link>
       </h1>
       <p>Data last updated: 30/01/2022</p>
